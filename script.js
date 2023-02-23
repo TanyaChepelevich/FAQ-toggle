f0c76a3a0b64da612dad02fdbb71a25f2c6c533f
Tanya Chepelevich <tanya.chepelevich@gmail.com>
2023-02-23 12:59:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll('.faq-toggle');
+buttons.forEach( button => {
+    button.addEventListener('click', () => {
+        button.parentNode.classList.toggle("active");
+    } )
+})
